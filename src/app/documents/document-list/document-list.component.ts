fc38cc3d3783c7d41facc9da333ed8a6dc6b40c7
unknown <robertdjonez@gmail.com>
2020-10-05 17:55:11 -0700

    Week 04

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'cms-document-list',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./document-list.component.css']
 })
 export class DocumentListComponent implements OnInit {
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
+
+  documents: Document[] = [
+    new Document('1', 'Text', 'Contains text document', 'www.text.com', null),
+    new Document('2', 'Powerpoint', 'Contains powerpoint', 'www.powerpoint.com', null),
+    new Document('3', 'PDF', 'Contains PDF', 'www.pdf.com', null),
+    new Document('4', 'Excel', 'Contains excel spreadsheet', 'www.excel.com', null),
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSelectedDocument(document: Document) {
+    this.selectedDocumentEvent.emit(document);
   }
 
 }
